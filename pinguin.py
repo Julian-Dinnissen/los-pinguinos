7b43f95b7602a24b4dd861840b5a53e5368c53e1
Julian <113358009+Julian-Dinnissen@users.noreply.github.com>
2024-04-03 00:16:12 +0200

    Update edit_json function parameters

--- pinguin.py
+++ pinguin.py
@@ -1,12 +1,12 @@
 import json
 
 
-def edit_json(new_years: list, new_population_data: list) -> None:
+def edit_json(new_years: list, new_data: list) -> None:
     with open("./static/data.json", "r") as file:
         data = json.load(file)
 
     data["years"] = new_years
-    data["populationData"] = new_population_data
+    data["populationData"] = new_data
 
     updated_json = json.dumps(data, indent=4)
 
@@ -20,5 +20,3 @@ new_years = ["2020", "2021", "2022", "2023", "2024", "2025", "2026"]
 new_population_data = [100, 120, 140, 160, 180, 200, 360]
 
 edit_json(new_years, new_population_data)
-
-
